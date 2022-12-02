f8438275b395dbcceff19b2e1fe3091b6b53c87b
Ilya <silvercrow735@gmail.com>
2022-12-02 15:15:36 +0300

    task2 complete

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите два числа a и b");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"a={a};b={b}->max={a}");
+}
+else
+{
+    if (a == b)
+    {
+        Console.WriteLine($"a=b={a}");
+    }
+    else
+    {
+        Console.WriteLine($"a={a};b={b}->max={b}");
+    }
+}
